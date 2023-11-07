b5fac6763adb7566a29ee54acb57f994048d2895
Prasadsiringula <124131837+Prasadsiringula@users.noreply.github.com>
2023-11-07 11:55:04 +0530

    Update conftest.py

--- Playwrightdemo/conftest.py
+++ Playwrightdemo/conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 @pytest.fixture()
 def setup_teardown(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False,executable_path="C:\Users\prasa\AppData\Local\Programs\Python\Python39\Lib\site-packages\playwright\driver\package\lib\server\chromium")
+    browser = playwright.chromium.launch(headless=False,executable_path=r"C:\Users\prasa\AppData\Local\Programs\Python\Python39\Lib\site-packages\playwright\driver\package\lib\server\chromium")
     context = browser.new_context()    #record_video_dir='./videos'
     page = context.new_page()
     page.goto("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
